5ccc2e8fcfc2e719a843c1c6e0258dc318ee78db
danieljlugo <daniellugofreelancer@gmail.com>
2023-01-23 14:36:03 -0300

    update button whatsapp

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -4,24 +4,24 @@ import imgCallToAction from "../assets/Project.svg";
 import ReactTyped from "react-typed";
 
 export default function CallToAction() {
-
-
-
-
-
-
   return (
     <div className="div-container-call">
       <div className="div-container-text-call p-5">
         {/* <h1 className="title-call py-2">Digitaliza tu negocio</h1> */}
-        <h1 className="title-call py-2">¿Quieres digitalizar tu negocio?</h1>
+        {/* <h1 className="title-call py-2">¿Quieres digitalizar tu negocio?</h1> */}
         {/* <h3>¿ Sos PYME o Emprendedor ?</h3> */}
-        <ReactTyped className="title-call py-2"
-                    strings={['¿Quieres digitalizar tu negocio?', 'Incrementar tu presencia online', '¿Llevarlo al siguiente nivel?', 'ARTSET Desarrollo Web y Digital ']}
-                    typeSpeed={50}
-                    backSpeed={50}
-                />
-      
+        <ReactTyped
+          className="title-call py-2"
+          strings={[
+            "¿Quieres digitalizar tu negocio?",
+            "Incrementar tu presencia online",
+            "¿Llevarlo al siguiente nivel?",
+            "ArtSet Desarrollo Web y Digital",
+          ]}
+          typeSpeed={50}
+          backSpeed={50}
+        />
+
         {/* <p className="text-call py-2">
           El equipo de <strong className="title-strong">Art Set</strong> tiene
           lo mejor para vos, incrementa valor a tu negocio con presencia en
@@ -29,7 +29,13 @@ export default function CallToAction() {
           y mucho más.{" "}
         </p> */}
         <p className="text-call py-2">
-        Si eres una PYME o emprendedor, <strong className="title-strong">el equipo de Art Set</strong> tiene lo que necesitas para mejorar tu presencia en internet y aumentar el valor de tu negocio. Ofrecemos servicios de desarrollo de sitios web, tiendas en línea, aplicaciones móviles y mucho más. Con nuestra experiencia y conocimientos, te ayudaremos a maximizar tu alcance en línea y a atraer a más clientes potenciales.
+          Si eres una PYME o emprendedor,{" "}
+          <strong className="title-strong">el equipo de Art Set</strong> tiene
+          lo que necesitas para mejorar tu presencia en internet y aumentar el
+          valor de tu negocio. Ofrecemos servicios de desarrollo de sitios web,
+          tiendas en línea, aplicaciones móviles y mucho más. Con nuestra
+          experiencia y conocimientos, te ayudaremos a maximizar tu alcance en
+          línea y a atraer a más clientes potenciales.
         </p>
         {/* <div className="py-3">
           <a href="www.google.com" className="nav-button button-call">
@@ -38,7 +44,7 @@ export default function CallToAction() {
         </div> */}
         <div className="py-3">
           <a href="#contact" className="nav-button button-call">
-          ¡Contáctanos hoy mismo! 
+            ¡Contáctanos hoy mismo!
           </a>
         </div>
       </div>
--- src/components/WhatsappContact.jsx
+++ src/components/WhatsappContact.jsx
@@ -9,9 +9,11 @@ export default function WhatsappContact() {
   return (
     <div className='floating-button' >
         
-            <a target="_blank" rel="noreferrer" href="https://wa.me/543518523090/?text=%20Hola,%20quisiera%20tener%20mayor%20información">
+            <a className='anchor-whatsapp' target="_blank" rel="noreferrer" href="https://wa.me/543518523090/?text=%20Hola,%20quisiera%20tener%20mayor%20información">
             <button className='btn-whatsapp'></button>
             </a>
+            <p className='text-whatsapp'>¡Estamos en WhatsApp!</p>
+            
         
     </div>
   )
