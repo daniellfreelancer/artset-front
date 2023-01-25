4827ddfe5153d1b9ce384ea4c2157198c31a1c36
danieljlugo <daniellugofreelancer@gmail.com>
2023-01-25 08:52:45 -0300

    update whatsapp button

--- src/components/WhatsappContact.jsx
+++ src/components/WhatsappContact.jsx
@@ -12,7 +12,6 @@ export default function WhatsappContact() {
             <a className='anchor-whatsapp' target="_blank" rel="noreferrer" href="https://wa.me/543518523090/?text=%20Hola,%20quisiera%20tener%20mayor%20información">
             <button className='btn-whatsapp'></button>
             </a>
-            <p className='text-whatsapp'>¡Estamos en WhatsApp!</p>
             
         
     </div>
